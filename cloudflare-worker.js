b80cf11639be53d84c72c16f99d3aa79a51841ff
Andreas Luckfiel <andreas@MacBook-Pro-M4.fritz.box>
2026-06-11 19:09:11 +0200

    Auto-sync: 11-06-2026 19:09

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -18,7 +18,7 @@
 
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin':  '*',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, X-Worker-Secret, X-Fotograaf-Token',
 };
 
